d515335f8ff1d555fcd31e0aeef74324fec8c339
Ina <ina.flaryanovich@gmail.com>
2024-11-05 01:02:00 +0100

    feat: define the GraphQL schema

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -1,5 +1,121 @@
 import { FastifyPluginAsyncTypebox, Type } from '@fastify/type-provider-typebox';
 import { getMemberTypeByIdSchema, memberTypeSchema } from './schemas.js';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+
+//- GraphQL schema
+const typeDefs = `
+  scalar UUID
+
+  # Enums
+  enum MemberTypeId {
+    BASIC
+    BUSINESS
+  }
+
+  # Input Types
+  input ChangePostInput {
+    title: String
+    content: String
+  }
+
+  input ChangeProfileInput {
+    isMale: Boolean
+    yearOfBirth: Int
+    memberTypeId: MemberTypeId
+  }
+
+  input ChangeUserInput {
+    name: String
+    balance: Float
+  }
+
+  input CreatePostInput {
+    title: String!
+    content: String!
+    authorId: UUID!
+  }
+
+  input CreateProfileInput {
+    isMale: Boolean!
+    yearOfBirth: Int!
+    userId: UUID!
+    memberTypeId: MemberTypeId!
+  }
+
+  input CreateUserInput {
+    name: String!
+    balance: Float!
+  }
+
+  # Types
+  type MemberType {
+    id: MemberTypeId!
+    discount: Float!
+    postsLimitPerMonth: Int!
+  }
+
+  type Post {
+    id: UUID!
+    title: String!
+    content: String!
+  }
+
+  type Profile {
+    id: UUID!
+    isMale: Boolean!
+    yearOfBirth: Int!
+    memberType: MemberType!
+  }
+
+  type User {
+    id: UUID!
+    name: String!
+    balance: Float!
+    profile: Profile
+    posts: [Post!]!
+    userSubscribedTo: [User!]!
+    subscribedToUser: [User!]!
+  }
+
+  # Root Query Type
+  type RootQueryType {
+    memberTypes: [MemberType!]!
+    memberType(id: MemberTypeId!): MemberType
+    users: [User!]!
+    user(id: UUID!): User
+    posts: [Post!]!
+    post(id: UUID!): Post
+    profiles: [Profile!]!
+    profile(id: UUID!): Profile
+  }
+
+  # Mutations
+  type Mutations {
+    createUser(dto: CreateUserInput!): User!
+    createProfile(dto: CreateProfileInput!): Profile!
+    createPost(dto: CreatePostInput!): Post!
+    changePost(id: UUID!, dto: ChangePostInput!): Post!
+    changeProfile(id: UUID!, dto: ChangeProfileInput!): Profile!
+    changeUser(id: UUID!, dto: ChangeUserInput!): User!
+    deleteUser(id: UUID!): String!
+    deletePost(id: UUID!): String!
+    deleteProfile(id: UUID!): String!
+    subscribeTo(userId: UUID!, authorId: UUID!): String!
+    unsubscribeFrom(userId: UUID!, authorId: UUID!): String!
+  }
+
+  # Schema Declaration
+  schema {
+    query: RootQueryType
+    mutation: Mutations
+  }
+`;
+
+//- create an executable schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  // resolvers,
+});
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma, httpErrors } = fastify;
